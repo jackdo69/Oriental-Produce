e68d87db8c56addd5b8986775c295dec4d4399b2
jackie <jackie.duc.do@gmail.com>
2017-03-23 11:03:11 +1100

    fixing posts

--- wp-content/themes/mystile/content.php
+++ wp-content/themes/mystile/content.php
@@ -10,7 +10,7 @@ if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename(
  */
 
 	global $woo_options;
- 
+
 /**
  * The Variables
  *
@@ -18,13 +18,13 @@ if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename(
  */
 
  	$settings = array(
-					'thumb_w' => 787, 
-					'thumb_h' => 300, 
+					'thumb_w' => 787,
+					'thumb_h' => 300,
 					'thumb_align' => 'aligncenter'
 					);
-					
+
 	$settings = woo_get_dynamic_values( $settings );
- 
+
 ?>
 
 	<article <?php post_class(); ?>>
@@ -36,24 +36,26 @@ if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename(
 			<span class="day"><?php the_time( 'd' ); ?></span>
 			<span class="year"><?php the_time( 'o' ); ?></span>
 		</aside>
-		
+
 		<section class="post-content">
-		    <?php 
-		    	if ( isset( $woo_options['woo_post_content'] ) && $woo_options['woo_post_content'] != 'content' ) { 
-		    		woo_image( 'width=' . $settings['thumb_w'] . '&height=' . $settings['thumb_h'] . '&class=thumbnail ' . $settings['thumb_align'] ); 
-		    	} 
+		    <?php
+		    	if ( isset( $woo_options['woo_post_content'] ) && $woo_options['woo_post_content'] != 'content' ) {
+		    		woo_image( 'width=' . $settings['thumb_w'] . '&height=' . $settings['thumb_h'] . '&class=thumbnail ' . $settings['thumb_align'] );
+		    	}
 		    ?>
-		    
+
 			<header>
-				<h1><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-				<?php woo_post_meta(); ?>
+				<h3><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+      <hr>
+				<!-- <?php woo_post_meta(); ?> -->
+        <section class="entry">
+  			<?php if ( isset( $woo_options['woo_post_content'] ) && $woo_options['woo_post_content'] == 'content' ) { the_content( __( 'Continue Reading &rarr;', 'woothemes' ) ); } else { the_excerpt(); } ?>
+  			</section>
 			</header>
-	
-			<section class="entry">
-			<?php if ( isset( $woo_options['woo_post_content'] ) && $woo_options['woo_post_content'] == 'content' ) { the_content( __( 'Continue Reading &rarr;', 'woothemes' ) ); } else { the_excerpt(); } ?>
-			</section>
-	
-			  
+
+
+
+
 		</section><!--/.post-content -->
 
 	</article><!-- /.post -->
